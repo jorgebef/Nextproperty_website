8754be0bbb7882a3a71138d4efadfb0d79b22558
Jorge Befan <jorge.befan@gmail.com>
2021-02-13 02:00:10 +0100

    implementing cookies on server

--- src/app.ts
+++ src/app.ts
@@ -43,8 +43,8 @@ app.use(
         // origin: [`${process.env.FRONT_URL}`, 'http://localhost:3000'],
         allowedHeaders: [
             'Access-Control-Allow-Credentials',
-            'Access-Control-Allow-Origin: *',
-            'Origin',
+            'Access-Control-Allow-Origin',
+            // 'Origin',
             'X-Requested-With',
             'Content-Type',
             'Accept',
