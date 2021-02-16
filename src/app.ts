53afe5e4b32f5aaf6b03a7a886228d815b538443
Jorge Befan <jorge.befan@gmail.com>
2021-02-16 20:48:16 +0100

    upload to server

--- src/app.ts
+++ src/app.ts
@@ -94,13 +94,13 @@ declare module 'express-session' {
 
 // Routes ===========================
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../nextproperty_frontend/build/index.html'), function (err) {
-        if (err) {
-            res.status(500).send(err);
-        }
-    });
-});
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../../nextproperty_frontend/build/index.html'), function (err) {
+//         if (err) {
+//             res.status(500).send(err);
+//         }
+//     });
+// });
 
 app.use(propertyRouter);
 app.use(loginRouter);
